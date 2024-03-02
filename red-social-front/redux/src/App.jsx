2bed1dfa31fffc1d2d61eeb1d1c2de1f021b9e73
AdrikLarreategui <adrian15186@hotmail.com>
2024-03-02 16:54:37 +0100

    commit creación AdminZone, PrivateZone, NotFound Y admin. Actualización App.jsx

--- red-social-front/redux/src/App.jsx
+++ red-social-front/redux/src/App.jsx
@@ -1,13 +1,20 @@
 import { useState } from 'react'
 import { React } from 'react'
-import Header from '../src/components/TheHeader/'
 import Login from '../src/components/Login/Login'
 import Register from '../src/components/Register/Register'
+import Profile from '../src/components/Profile/Profile'
 import Home from './components/Home/Home'
 import TheHeader from './components/TheHeader/TheHeader'
-import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import PostDetail from './components/Posts/PostDetail/PostDetail'
+import Search from './components/Search/Search'
+import AdminZone from './guards/AdminZone'
+import Admin from './components/Admin/Admin'
+import PrivateZone from './guards/PrivateZone'
+import NotFound from './components/NotFound/NotFound'
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
+
 import './App.css'
+
 
 function App() {
   return (
@@ -18,9 +25,11 @@ function App() {
           <Route path='/' element= {<Home />} />
           <Route path='login' element={ <Login /> } />
           <Route path='/register' element={ <Register /> } />
-          <Route path='/profile' element= { <Profile /> } />
+          <Route path="/profile" element={<PrivateZone> <Profile /></PrivateZone>}/>
+          <Route path='/admin' element= {<AdminZone> <Admin /> </AdminZone> } />
           <Route path='/post/:id' element= { <PostDetail />} />
           <Route path='/search/:postName' element = { <Search /> } />
+          <Route path= '*' element= {<NotFound />}/>
         </Routes>
       </BrowserRouter>
     </div>
